7460c429fcce7acefce81553df487865179b8761
Henrique de Souza Linhares <43452663+hslinhares@users.noreply.github.com>
2025-03-07 17:46:54 -0300

    cria uo_sigla_current no transform (#89)
    * cria uo_sigla_current no transform
    
    * ajusta formatacao do codigo scripts/tranform.py
    
    * Update setuptools in Dockerfile
    
    See https://github.com/splor-mg/dados-armazem-siafi/issues/39
    See https://github.com/splor-mg/dados-armazem-siafi/commit/c7eb8e37d207be32caf43ace3603c278d82d9b8b
    
    * Add prints to transform scripts
    
    See https://github.com/splor-mg/roperacional/issues/49
    See https://chatgpt.com/share/67ca1776-63a0-8003-b8e9-4ee94d18d74b
    
    ---------
    
    Co-authored-by: Gabriel Dornas <gabrielbdornas@gmail.com>

--- scripts/transform.py
+++ scripts/transform.py
@@ -6,16 +6,17 @@ from scripts.pipelines import transform_pipeline
 
 logger = logging.getLogger(__name__)
 
+# resource_name = 'uo' # para rodar no modo interativo
+
 def transform_resource(resource_name: str, source_descriptor: str = 'datapackage.yaml'):
     logger.info(f'Transforming resource {resource_name}')
-    
+
     package = Package(source_descriptor)
     resource = package.get_resource(resource_name)
-
-    schema = Schema.from_descriptor(f'schemas/{resource.name}.yaml')
-
+    schema = Schema.from_descriptor(f'schemas/{resource_name}.yaml')
     resource.transform(transform_pipeline)
     table = resource.to_petl()
+
     for field in resource.schema.fields:
         target = field.custom.get('target')
         target = target if target else as_identifier(field.name)
@@ -23,7 +24,7 @@ def transform_resource(resource_name: str, source_descriptor: str = 'datapackage
         table = etl.select(table, "ano", lambda v: v >= 2008)
 
     if resource_name == "elemento_item":
-     
+
         table = etl.selectnotnone(table, "elemento_item_desc")
         table = etl.distinct(table)
 
@@ -31,6 +32,24 @@ def transform_resource(resource_name: str, source_descriptor: str = 'datapackage
         table = etl.select(
             table, lambda row: row.ano == 2023 and row.uo_cod == 1401 and row.funcao_cod == 12 and row.acao_cod == 4302, complement = True
         )
+
+    if resource_name == "uo":
+        table = etl.convert(table, 'ano', int)
+        table = etl.convert(table, 'uo_cod', int)
+        max_uo_cod_ano = table.cut('uo_cod', 'ano').rowreduce(key='uo_cod', reducer=lambda key, rows: (key, max(row['ano'] for row in rows)), header=['uo_cod', 'max_ano'])
+
+        print("max_uo_cod_ano before join:")
+        print(list(etl.data(max_uo_cod_ano.cut('uo_cod', 'max_ano'))))  # Ensure these columns exist
+
+        print("table before join:")
+        print(list(etl.data(table.cut('uo_cod', 'ano', 'uo_sigla'))))
+
+        uo_sigla_current = etl.leftjoin(max_uo_cod_ano, table, lkey=['uo_cod', 'max_ano'], rkey=['uo_cod', 'ano']).cut('uo_cod', 'uo_sigla').rename('uo_sigla', 'uo_sigla_current').cache()
+
+        print("uo_sigla_current after join:")
+        print(list(etl.data(uo_sigla_current)) if uo_sigla_current else "None")
+
+        table = table.leftjoin(uo_sigla_current, key='uo_cod')
 
     table = etl.addfield(table, f'chave_{resource.name}', lambda row: '|'.join(str(row[key]) for key in schema.primary_key), index=0)
 
