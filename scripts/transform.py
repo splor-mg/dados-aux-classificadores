c0dbc1951436958ea318d8e6ce47f320f89dc0ed
Henrique Linhares <henrique.s.linhares@gmail.com>
2025-03-10 15:52:28 -0300

    retira elementos itens duplicados para o mesmo ano

--- scripts/transform.py
+++ scripts/transform.py
@@ -26,7 +26,7 @@ def transform_resource(resource_name: str, source_descriptor: str = 'datapackage
     if resource_name == "elemento_item":
 
         table = etl.selectnotnone(table, "elemento_item_desc")
-        table = etl.distinct(table)
+        table = etl.distinct(table, key=('ano', 'elemento_item_cod'))
 
     if resource_name == "funcional_programatica":
         table = etl.select(
